e6ea02b4a388de1a641e566f481ceae5135bd2a1
PLdiaz <igeo.diaz@gmail.com>
2026-04-29 12:27:38 -0500

    Primer visor GIS funcionando

---
+++ script.js
@@ -0,0 +1,33 @@
+const map = L.map('map').setView([-12.0464, -77.0428], 12);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 22,
+  attribution: '© OpenStreetMap'
+}).addTo(map);
+
+fetch('data/predios.geojson')
+  .then(response => response.json())
+  .then(data => {
+    const capa = L.geoJSON(data, {
+      style: {
+        color: '#ff0000',
+        weight: 2,
+        fillColor: '#ff6666',
+        fillOpacity: 0.4
+      },
+      onEachFeature: function (feature, layer) {
+        let popup = '';
+
+        if (feature.properties) {
+          for (const campo in feature.properties) {
+            popup += `<b>${campo}:</b> ${feature.properties[campo]}<br>`;
+          }
+        }
+
+        layer.bindPopup(popup);
+      }
+    }).addTo(map);
+
+    map.fitBounds(capa.getBounds());
+  })
+  .catch(error => console.log(error));
